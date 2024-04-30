41031c2bcede0220d5932adb978557b1f28e2295
Heits <heits.software@gmail.com>
2024-04-29 22:05:14 -0400

    downloaded pico v2.0.6, initialized npm directory

--- header.php
+++ header.php
@@ -8,8 +8,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css"> -->
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="node_modules/@picocss/pico/css/pico.lime.min.css">
+    
     <link rel="icon" type="image/x-icon" href="assets/New Logo Design.svg">
     <title>Tacos On Mars</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
